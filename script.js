51178137a9031f3bfa190c3039d7655458222d19
Pavan Teja Reddy <142150313+PavanTejaReddy1@users.noreply.github.com>
2025-09-19 23:08:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-function volume_sphere() {
-    //Write your code here
-  
+function volume_sphere(e) {
+	e.preventDefault();
+	const radius = document.getElementById("radius").value;
+	const volume = document.getElementById("volume");
+	volume.value = ((4/3)*Math.PI*Math.pow(radius, 3)).toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
